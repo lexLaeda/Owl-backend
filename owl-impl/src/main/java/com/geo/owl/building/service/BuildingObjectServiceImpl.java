b2fcc89a12f1a94a694c9f255975c7e0f6928d12
AlexeyErmakov <lexlaeda30@gmail.com>
2021-01-09 18:29:00 +0300

    OS-5 add log.log

--- owl-impl/src/main/java/com/geo/owl/building/service/BuildingObjectServiceImpl.java
+++ owl-impl/src/main/java/com/geo/owl/building/service/BuildingObjectServiceImpl.java
@@ -1,14 +1,23 @@
 package com.geo.owl.building.service;
 
 import com.geo.owl.building.dto.BuildingObjectDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
+@Slf4j
 @Service
 public class BuildingObjectServiceImpl implements BuildingObjectService {
+
+    @PostConstruct
+    public void init(){
+        log.info("Ура!!!!!");
+    }
+
     @Override
     public BuildingObjectDto createBuildingObject(BuildingObjectDto buildingObject) {
         return null;
