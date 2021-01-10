370835da89f8315c4b3a4c9d121dfef072aaf1ce
AlexeyErmakov <lexlaeda30@gmail.com>
2021-01-10 12:13:10 +0300

    OS-7

--- owl-impl/src/main/java/com/geo/owl/common/config/DataSourceConfig.java
+++ owl-impl/src/main/java/com/geo/owl/common/config/DataSourceConfig.java
@@ -1,10 +1,8 @@
 package com.geo.owl.common.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
